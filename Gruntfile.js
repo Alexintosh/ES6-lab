fcc8e5314960133b2eeb02c294de46f1033e2e64
Alessio <alessio.d@gmail.com>
2015-12-11 18:04:16 +0000

    Let, classes, multiline strings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,42 @@
-require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
-
-grunt.initConfig({
-    "babel": {
-          options: {
-                           sourceMap: true
+module.exports = function (grunt) {
+   grunt.initConfig({
+      browserify: {
+         dist: {
+            options: {
+               transform: [
+                  ["babelify", {
+                     loose: "all"
+                  }]
+               ]
+            },
+            files: {
+               // if the source file has an extension of es6 then
+               // we change the name of the source file accordingly.
+               // The result file's extension is always .js
+               "./dist/module.js": ["./modules/index.js"]
+            }
+         }
       },
-      dist: {
-                    files: {
-                                     "dist/app.js": "src/app.js"
+      watch: {
+         scripts: {
+            files: ["./modules/*.*"],
+            tasks: ["browserify"]
+         }
+      },
+      connect: {
+        server: {
+          options: {
+            port: 9000,
+            base: '.'
+          }
         }
-                               }
-  }
-});
+      }
+   });
 
-grunt.registerTask("default", ["babel"]);
+   grunt.loadNpmTasks("grunt-browserify");
+   grunt.loadNpmTasks("grunt-contrib-watch");
+   grunt.loadNpmTasks('grunt-contrib-connect');
+
+   grunt.registerTask("build", ["browserify"]);
+   grunt.registerTask("default", ["connect", "watch"]);
+};
---
+++ modules/index.js
@@ -0,0 +1,82 @@
+
+/**
+ * Let
+ * */
+/*
+let dog = "Puppy"
+
+while(true){
+  let dog = "Pupa"
+  console.log(dog)
+  break;
+}
+
+console.log(dog)
+*/
+
+/**
+ * Classes, super, static
+ * */
+
+/*
+class Dog {
+  constructor(){
+    this.name = "Puppy"
+  }
+
+  say(){
+    console.log( this.name + ' say hi')
+  }
+
+  bark(){
+    console.log("woff woff")
+  }
+
+  static whoIam( ){
+    console.log( "I'm the " +this.name+ " class" )
+  }
+}  
+
+let Billo = new Dog()
+Billo.say()
+Dog.whoIam()
+
+class Labrador extends Dog{
+  constructor(name){
+    super()
+    this.name = name
+    this.type = "labrador"
+  }  
+
+  say(){
+    console.log( this.name + ' say hi, I\'m a ' + this.type)
+  }
+}
+
+let Fido = new Labrador("Fido")
+Fido.bark()
+Fido.say()
+*/
+
+/**
+ * Template strings
+ * */
+
+// Multiline strings
+console.log(`In es5 this is
+ not legal.`)
+
+// String interpolation
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`)
+
+/**
+ * Arrow functions
+ * */
+var sum = (a, b = 6) => (a + b);
+
+var square = (b) => {
+      return b * b;
+};
+
+console.log(sum(1,9));
