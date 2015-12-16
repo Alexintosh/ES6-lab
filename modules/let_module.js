e28e0a31c1c6803b3f557b9efe5b8db98653df50
Alessio <alessio.d@gmail.com>
2015-12-16 11:07:47 +0000

    Adding let modules

---
+++ modules/let_module.js
@@ -0,0 +1,17 @@
+/**
+ * Let
+ * */
+
+function letExample(){
+  let dog = "Puppy"
+
+  while(true){
+    let dog = "Pupa"
+    console.log(dog)
+    break;
+  }
+  
+  console.log(dog)
+}
+
+export default {letExample}
